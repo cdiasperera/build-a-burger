b97632e660b5138bbec7f2a3c2ed7872b3d3219d
Channa <cdiasperera@gmail.com>
2019-12-21 10:41:56 +0530

    Added a Layout component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import Layout from './components/Layout'
 
 function App () {
   return (
-    <div />
+    <>
+      <Layout>
+        <p>Test</p>
+      </Layout>
+    </>
   )
 }
 
