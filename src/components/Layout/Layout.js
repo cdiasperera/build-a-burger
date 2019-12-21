2e4bca2d88291e6b3f75a324459af008962935c9
Channa <cdiasperera@gmail.com>
2019-12-21 11:04:09 +0530

    Added burger container

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import classes from './Layout.module.css'
 
 const layout = (props) => {
   return (
     <>
-      <div>
+      <div className={classes.toolbar}>
         Toolbar, SideDrawer, Backdrop
       </div>
       <main>
---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class BurgerBuilder extends Component {
+  render = () => {
+    return (
+      <>
+        <div>Burger</div>
+        <div>BuildControls</div>
+      </>
+    )
+  }
+}
+
+export default BurgerBuilder
