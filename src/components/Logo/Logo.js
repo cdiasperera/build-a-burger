1adca4edead2e0e827d7cf912d6e39d575baedf8
Channa <cdiasperera@gmail.com>
2020-01-03 20:49:58 +0530

    Link added to logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,12 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import classes from './Logo.module.css'
 import logoImg from '../../assets/logo512.png'
 const Logo = ({ className }) => {
   return (
-    <div className={[className, classes.Logo].join(' ')}>
-      <img src={logoImg} alt='logo' />
-    </div>
+    <Link to='/'>
+      <div className={[className, classes.Logo].join(' ')}>
+        <img src={logoImg} alt='logo' />
+      </div>
+    </Link>
   )
 }
 
