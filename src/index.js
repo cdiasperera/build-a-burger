6130a535774a9943ff3051c936253fccc7dea81f
Channa <cdiasperera@gmail.com>
2020-01-08 12:30:28 +0530

    Applied thunk

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
 
 import './index.css'
 import App from './App'
@@ -9,9 +10,11 @@ import App from './App'
 import * as serviceWorker from './serviceWorker'
 import reducer from './store/reducer'
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
 const store = createStore(
   reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnhancers(applyMiddleware(thunk))
 )
 
 ReactDOM.render(
